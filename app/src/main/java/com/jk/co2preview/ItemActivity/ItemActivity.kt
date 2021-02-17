ee5bce5fd4e2376285bd7e23ea37a4a9fa243336
jk12 <jk@hades.ai>
2021-02-17 20:02:27 +0100

    added database and viewing support

--- app/src/main/java/com/jk/co2preview/ItemActivity/ItemActivity.kt
+++ app/src/main/java/com/jk/co2preview/ItemActivity/ItemActivity.kt
@@ -2,9 +2,14 @@ package com.jk.co2preview.ItemActivity
 
 import android.app.Activity
 import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import android.widget.TextView
 import com.jk.co2preview.R
 import com.jk.co2preview.data_representation.BuyItem
+import com.jk.co2preview.database.DBHandler
+import com.jk.co2preview.database.DatabaseItem
+
 
 class ItemActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,11 +21,42 @@ class ItemActivity : Activity() {
         val priceTextView = findViewById<TextView>(R.id.item_price)
         val quantTextView = findViewById<TextView>(R.id.item_quant)
         val reductionTextView = findViewById<TextView>(R.id.item_reduction)
+        val linkTextView = findViewById<TextView>(R.id.item_link)
+
 
         nameTextView.text = item.name
         priceTextView.text = item.price.toString()
         quantTextView.text = item.quantity.toString()
         reductionTextView.text = item.action.toString()
+
+        val dbHandler = DBHandler(this, null, null, 1)
+
+        val product: DatabaseItem? = item.name?.let { dbHandler.findProduct(it) }
+
+        if(product != null) {
+            val nutrients: MutableList<List<String>>? = product.get_nutrients()
+            if (nutrients != null) {
+                for (i in nutrients.indices) {
+                    val inner: List<String> = nutrients[i]
+                    for (j in inner.indices) {
+                        val textView = findViewById<TextView>(resources.getIdentifier("z$i$j", "id", this.getPackageName()))
+                        textView.text = inner[j]
+
+                    }
+
+                }
+            }
+            val link: String? = product.get_link()
+            if(link != null){
+                linkTextView.setOnClickListener{
+                    setContentView(R.layout.item_webview)
+                    val webView: WebView = findViewById(R.id.webview)
+                    webView.settings.javaScriptEnabled = true
+                    webView.webViewClient = WebViewClient()
+                    webView.loadUrl(link)
+                }
+            }
+        }
 
 
     }
---
+++ app/src/main/java/com/jk/co2preview/database/DatabaseItem.kt
@@ -0,0 +1,40 @@
+package com.jk.co2preview.database
+
+class DatabaseItem{
+    private var id : Int = -1
+    private var name: String? = null
+    private var gen_info: String? = null
+    private var desc: String? = null
+    private var nutrients: String? = null
+    private var origin: String? = null
+    private var price: Float? = null
+    private var orig_price: Float? = null
+    private var link: String? = null
+
+    constructor(id: Int, name: String?, desc: String?, gen_info: String?, nutrients: String?, origin: String?, price: Float?, orig_price: Float?, link: String?){
+        this.id = id
+        this.name= name
+        this.desc= desc
+        this.gen_info= gen_info
+        this.nutrients= nutrients
+        this.origin= origin
+        this.price= price
+        this.orig_price= orig_price
+        this.link= link
+    }
+
+    fun get_nutrients(): MutableList<List<String>>? {
+        if(nutrients == null){
+            return null
+        }
+        var res : MutableList<List<String>> = emptyList<List<String>>().toMutableList()
+        for(nut in this.nutrients?.split(';')!!.dropLast(1)){
+            res.add(nut.split(','))
+        }
+        return res
+    }
+
+    fun get_link(): String?{
+        return this.link
+    }
+}
